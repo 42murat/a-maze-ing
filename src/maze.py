d0a09133e91053e100a0606f4d0597e85b890914
Jozef Bugajewski <jbugajew@c3r10s1.42warsaw.pl>
2026-06-25 17:38:21 +0200

    ADD: Finish fill_all_cells() and set_42_pattern(). The first function initialize all maze as accupied squares and the second check if is it possible to have 42 pattern in maze and if so then the status of cells that are part of it is set to value that prevent them form being in any path in maze.

--- src/maze.py
+++ src/maze.py
@@ -49,7 +49,13 @@ class Maze:
 
             def fill_all_cells(self) -> None:
                 """Set all maze cells to fully closed (all walls occupied)."""
-                ...
+                cell_sub_maze_id: int = 0
+                for y in range(self.maze.parameters.height):
+                    row: list[Maze.Cell] = []
+                    for x in range(self.maze.parameters.width):
+                        row.append(Maze.Cell(x, y, cell_sub_maze_id))
+                        cell_sub_maze_id += 1
+                    self.maze.cells.append(row)
 
             def set_42_pattern(self) -> None:
                 """Set some maze cells sub_maze_id to value that prevents them form
@@ -57,7 +63,60 @@ class Maze:
                 
                 This is to satisfy condition:
                     The maze must contain a visible “42” drawn
-                    by several fully closed cells."""
+                    by several fully closed cells.
+                    
+                For now I assume 42 will be drawm in 7x5 area, in the center of the maze.
+                If maze height and width are odd values the patter in perfectly centered,
+                if one of them is even the pattern is shifted by 1 cell to the left or up.
+                
+                Because of that is gennerally better to avoid maze input and output to be
+                whole ractangle and in region near it.
+                
+                If the 42 parrert is in conflict with maze entry or exit or maze size is too small
+                the pattern will not be drawn in this maze."""
+                pattern_42: list[list[int]] = [
+                    [1, 0, 0, 0, 1, 1, 1],
+                    [1, 0, 0, 0, 0, 0, 1],
+                    [1, 1, 1, 0, 1, 1, 1],
+                    [0, 0, 1, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 1, 1, 1]
+                ]
+                def check_entry_exit_in_42_pattern() -> bool:
+                    """Checks whether maze entry or exit is in 42 pattern."""
+                    for y, row in enumerate(reversed(pattern_42)):
+                        for x, pattern in enumerate(row):
+                            if pattern == 1:
+                                if (
+                                    (pattern_x_offset + x == (self.maze.parameters.entry_x or self.maze.parameters.exit_x))
+                                    and (pattern_y_offset + y == (self.maze.parameters.entry_y or self.maze.parameters.exit_y))
+                                ):
+                                    return True
+                    return False
+
+                if self.maze.parameters.width < 9 or self.maze.parameters.height < 7:
+                    print(
+                        "Maze width must be at least 9 and height at least 7 to draw 42 pattern."
+                        "The 42 pattern will not be drawn in this maze."
+                    )
+                    return
+                pattern_x_offset: int = (self.maze.parameters.width - 7) // 2
+                pattern_y_offset: int = (self.maze.parameters.height - 5 + 1) // 2
+                #FIXME: For now to see change I make all cells open.
+                
+                if check_entry_exit_in_42_pattern():
+                    print(
+                        "Maze entry or exit is in 42 pattern area." \
+                        "The 42 pattern will not be drawn in this maze."
+                    )
+                    return
+                for y, row in enumerate(reversed(pattern_42)):
+                    for x, pattern in enumerate(row):
+                        if pattern == 1:
+                            self.maze.get_cell(pattern_x_offset + x, pattern_y_offset + y).sub_maze_id = -1
+                            # self.maze.get_cell(pattern_x_offset + x, pattern_y_offset + y).N_wall = False
+                            # self.maze.get_cell(pattern_x_offset + x, pattern_y_offset + y).E_wall = False
+                            # self.maze.get_cell(pattern_x_offset + x, pattern_y_offset + y).S_wall = False
+                            # self.maze.get_cell(pattern_x_offset + x, pattern_y_offset + y).W_wall = False
 
             def generate_perfect_maze(self) -> None:
                 """Generates perfect maze (maze with no loops). All cells (except)
@@ -109,21 +168,35 @@ class Maze:
         def get_status(self) -> str:
             """Returns cell walls status in hex (string)."""
             return hex(self.N_wall<<0 | self.E_wall<<1 | self.S_wall<<2 | self.W_wall<<3)[2:].upper()
+        
+    def get_cell(self, x: int, y: int) -> Cell:
+        """Returns maze cell at coordinates (x, y)."""
+        return self.cells[y][x]
 
     def get_maze_hex(self) -> list[list[str]]:
         """Returns list list of strings, the outer list repesents maze rows,
         the inner maze cells in single row. Inner value is string representing
-        cell status (what walls are occupied) in hex (string)."""
+        cell status (what walls are occupied) in hex (string).
+        
+        When displaying the  maze rows viev  is form top to bottom, and when
+            dispalying maze cells in single row view is from left to right."""
         #FOR NOW IM MOCKING THIS FUNCTION BECAUSE OTHER FUNCTIONS DEPENDS ON IT.
         #In example-maze.jpg there is what this maze looks like
         #THIS WHAT THIS FUCNTION COULD RETURN:
-        return [
-            ["B", "D", "5", "3", "B"],
-            ["C", "1", "7", "A", "A"],
-            ["D", "2", "9", "6", "A"],
-            ["9", "2", "A", "9", "2"],
-            ["E", "C", "4", "6", "E"]
-        ]
+        # return [
+        #     ["B", "D", "5", "3", "B"],
+        #     ["C", "1", "7", "A", "A"],
+        #     ["D", "2", "9", "6", "A"],
+        #     ["9", "2", "A", "9", "2"],
+        #     ["E", "C", "4", "6", "E"]
+        # ]
+        result: list[list[str]] = []
+        for row in reversed(self.cells):
+            result_row: list[str] = []
+            for cell in row:
+                result_row.append(cell.get_status())
+            result.append(result_row)
+        return result
 
     def get_output_file_path(self) -> str:
         """Returns file path to which export maze."""
