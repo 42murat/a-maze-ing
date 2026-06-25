671b7ec93bc65806cc992445ddcde1249227ed7a
Jozef Bugajewski <jbugajew@c3r10s1.42warsaw.pl>
2026-06-25 16:00:21 +0200

    UPDATE: launch.json to be able run new a_maze_ing.py that is conside to be main program. ADD:MazeParameter class contains now maze values. Temporary is mocked with statring values. ADD:MazeGeneratorClass - it will contain functions that will generate maze base on its input parameters. ADD: Cell class - it can now be created end return its status (hex-str).

--- src/maze.py
+++ src/maze.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
 class MazeParameters:
     """Probably will contain all config file values, like.
     
@@ -7,27 +10,105 @@ class MazeParameters:
         ...
         self.visualize: bool = visualize
         ..."""
-    ...
-    #FOR NOW TO MAKE OTHER PARTS PASS
-    def __init__(self):
-        self.visualize = True
+    #FIXME: For now I'm mocking arguments, because other functions depends on it.
+    def __init__(self,
+                 width: int = 10,
+                 height: int = 10,
+                 entry_x: int = 0,
+                 entry_y: int = 0,
+                 exit_x: int = 9,
+                 exit_y: int = 9,
+                 output_file_path: str = "output_maze.txt",
+                 perfect: bool = True,
+                 visualize: bool = True
+                ):
+        self.width: int = width
+        self.height: int = height
+        self.entry_x: int = entry_x
+        self.entry_y: int = entry_y
+        self.exit_x: int = exit_x
+        self.exit_y: int = exit_y
+        self.output_file_path: str = output_file_path
+        self.perfect: bool = perfect
+        self.visualize: bool = visualize
 
 class Maze:
     def __init__(self, parameters: MazeParameters):
         """Initialize maze object - it's content (cells) is not initialized yet."""
-        self.parameters = parameters
+        self.parameters: MazeParameters = parameters
+        self.cells: list[list[Maze.Cell]] = []
     
 
     def generate(self) -> None:
         """Cretes maze - do all stuff we talked about, like creating sollution path,
         filling the rest of maze, etc. Returns Maze class object."""
         ...
+        class MazeGenerator:
+            def __init__(self, maze: Maze):
+                self.maze: Maze = maze
 
-    class cell:
-        """Maze cell, contains x, y coordinates and cell status (what walls are occupied)"""
+            def fill_all_cells(self) -> None:
+                """Set all maze cells to fully closed (all walls occupied)."""
+                ...
+
+            def set_42_pattern(self) -> None:
+                """Set some maze cells sub_maze_id to value that prevents them form
+                being part of maze during generation. 
+                
+                This is to satisfy condition:
+                    The maze must contain a visible “42” drawn
+                    by several fully closed cells."""
+
+            def generate_perfect_maze(self) -> None:
+                """Generates perfect maze (maze with no loops). All cells (except)
+                cells in 42 pattern are part of maze. There is only one path
+                connecting maze start with maze exit."""
+                ...
+
+            def remove_some_walls(self) -> None:
+                """Removes some walls from maze to create loops in maze.
+                
+                This is take into account condition:
+                    The maze can't have large open areas. Corridors can't be wider
+                    than 2 cells. For example, you can have 2x3 or 3x2 open area,
+                    but never a 3x3 open area."""
+                ...
+
+        generator = MazeGenerator(self)
+
+        generator.fill_all_cells()
+        generator.set_42_pattern()
+        generator.generate_perfect_maze()
+        if not self.parameters.perfect:
+            generator.remove_some_walls()
+
+
+    class Cell:
+        """Maze cell, contains x, y coordinates and cell status (what walls are occupied).
+        The sub_maze_id is used to identify which sub-maze this cell belongs to. 
+        This is useful for generating perfect mazes."""
         ...
-        def get_status() -> str:
+        def __init__(
+                self,
+                x: int,
+                y: int,
+                sub_maze_id: int,
+                N_wall: bool = True,
+                E_wall: bool = True,
+                S_wall: bool = True,
+                W_wall: bool = True
+        ):
+            self.x: int = x
+            self.y: int = y
+            self.sub_maze_id: int = sub_maze_id
+            self.N_wall: bool = N_wall
+            self.E_wall: bool = E_wall
+            self.S_wall: bool = S_wall
+            self.W_wall: bool = W_wall
+
+        def get_status(self) -> str:
             """Returns cell walls status in hex (string)."""
+            return hex(self.N_wall<<0 | self.E_wall<<1 | self.S_wall<<2 | self.W_wall<<3)[2:].upper()
 
     def get_maze_hex(self) -> list[list[str]]:
         """Returns list list of strings, the outer list repesents maze rows,
